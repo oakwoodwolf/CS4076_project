ac8f6209ff1d17ad23641c2c09c6443844fa55f9
Roy Flaherty <royfla100@gmail.com>
2023-03-09 14:50:04 +0000

    adding button functionality

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,31 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QUrl>
+#include <QDesktopServices> // for opening a link
+#include <QPixmap>
+
+QUrl currentUrl = QUrl("https://www.bbcgoodfood.com/recipes/strawberry-cheesecake-4-easy-steps", QUrl::TolerantMode);
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
+
 {
+    QPixmap recipePic ("../res/pics/strawberry-cheesecake-11.jpg");
     ui->setupUi(this);
+    ui->recipeImage->setPixmap(recipePic);
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+     QDesktopServices::openUrl(currentUrl);
 }
 
