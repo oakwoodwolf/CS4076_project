b141899c6839db8465ae990744f722af884e99f1
Roy Flaherty <royfla100@gmail.com>
2023-03-27 17:43:01 +0100

    made ingredient abstract

--- ingredient.h
+++ ingredient.h
@@ -6,6 +6,11 @@ class Ingredient
 {
 public:
     Ingredient(QString name, float capacity) : name(name), capacity(capacity) {}
+    virtual QString getCapacity() = 0; //Subclasses handle this
+    virtual QString displayIngredient() = 0;
+    QString getName(){
+        return name;
+    }
 private:
     QString name;
     float capacity;
